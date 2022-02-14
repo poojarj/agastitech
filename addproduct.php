0fc2a84f2c5d04b1e008876dd91c43a60d1b29d9
pooja jadhav <poojakhawle44@gmail.com>
2022-02-14 17:35:28 +0530

    i have commet it

--- addproduct.php
+++ addproduct.php
@@ -7,19 +7,49 @@ $company=$_POST['company'];
 $ngadget=$_POST['ngadget'];
 $price=$_POST['price'];
 $size=$_POST['size'];
+$file=$_FILES['image'];
+  //  print_r ($file);
 
-$sql = "INSERT INTO addprductinfo (gadget,company,ngadget,price,size)
-VALUES ('$gadget','$company','$ngadget',$price,$size)";
-if ($conn->query($sql) === TRUE) {
+$filename=$file['name'];
+$filepath=$file['tmp_name'];
+$error=$file['error'];
+
+
+if($error==0){
+
+$destfile='uplode/'.$filename;
+//echo "$destfile";
+move_uploaded_file($filepath,$destfile);
+
+$sql = "INSERT INTO addprductinfo (gadget,company,ngadget,price,size,pic)
+VALUES ('$gadget','$company','$ngadget','$price','$size','$destfile')";
+
+$query =mysqli_query($con,$sql);
+
+// echo $query;
+
     // echo "New record created successfully";
     echo'<script type="text/javascript">
 alert("New record created successfully!");
-window.location.href = "index.php";
+window.location.href = "store.php";
 </script>';
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  }
+} else{
+  echo "not uplode";
+}
+
+
+// $sql = "INSERT INTO addprductinfo (gadget,company,ngadget,price,size)
+// VALUES ('$gadget','$company','$ngadget',$price,$size)";
+// if ($conn->query($sql) === TRUE) {
+//     // echo "New record created successfully";
+//     echo'<script type="text/javascript">
+// alert("New record created successfully!");
+// window.location.href = "index.php";
+// </script>';
+//   } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+//   }
   
-  $conn->close();
+   $con->close();
   
 ?>
